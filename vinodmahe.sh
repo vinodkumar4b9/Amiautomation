dcc0fd2ab368a54e403c0f1e874a93b181afcb38
vinodkumar4b9 <vinodkumar4b9@gmail.com>
2020-10-16 12:46:54 +0530

    Update vinodmahe.sh

--- vinodmahe.sh
+++ vinodmahe.sh
@@ -1,4 +1,4 @@
-curl -H "Authorization: bearer af7d93fe4f7f9d7b0585fb62cff4511b1f4c0a80 " -X POST -d " \
+curl -H "Authorization: bearer a8add034210fd73ccc7e141fddb8c2c21a625ee2 " -X POST -d " \
  { \
    \"query\": \"query { viewer { login }}\" \
  } \
